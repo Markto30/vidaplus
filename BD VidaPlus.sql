964fd2205a71e794ad28fc28ab5c644740433842
Markto30 <141197365+Markto30@users.noreply.github.com>
2025-07-13 16:16:56 -0300

    Add files via upload
    Adicionando versao atual do mysql para consulta

--- BD VidaPlus.sql
+++ BD VidaPlus.sql
@@ -1,13 +1,32 @@
-CREATE DATABASE IF NOT EXISTS vidaplus;
+-- Criar o banco de dados
 USE vidaplus;
 
+-- Criar a tabela 'usuarios' com a hierarquia atualizada
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome_completo VARCHAR(100) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     telefone VARCHAR(20),
     endereco VARCHAR(150),
-    usuario_id VARCHAR(50) NOT NULL UNIQUE,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    hierarquia ENUM('Administrador', 'Enfermeiro', 'Paciente') NOT NULL
+    hierarquia ENUM('Administrador', 'Medico', 'Paciente') NOT NULL
 );
+
+SELECT * FROM usuarios;
+
+CREATE TABLE agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    paciente_usuario VARCHAR(50),
+    medico_usuario VARCHAR(50),
+    data_consulta DATE,
+    hora_consulta TIME,
+    observacoes TEXT,
+    FOREIGN KEY (paciente_usuario) REFERENCES usuarios(usuario),
+    FOREIGN KEY (medico_usuario) REFERENCES usuarios(usuario)
+);
+
+SELECT * FROM agendamentos
+
+
+
